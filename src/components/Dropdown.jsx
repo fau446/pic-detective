fbfaf1a5c1bca2b8dcf731a5153534afada3941d
Franklin <Franklinau2@gmail.com>
2024-05-06 13:34:36 -0700

    Add Dropdown styling

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,15 +2,19 @@ import styles from "../styles/Dropdown.module.css";
 
 function Dropdown({ coords, options, handleOptionSelect }) {
   const filteredOptions = options.filter((option) => !option.found);
-
   return (
     <div className={styles.dropdown} style={{ left: coords.x, top: coords.y }}>
       <ul>
         {filteredOptions.map((option) => {
           return (
-            <li key={option._id} onClick={() => handleOptionSelect(option._id)}>
-              {option.name}
-            </li>
+            <div
+              className={styles.option}
+              key={option._id}
+              onClick={() => handleOptionSelect(option._id)}
+            >
+              <img className={styles.icon} src={"../../" + option.img_name} />
+              <li key={option._id}>{option.name}</li>
+            </div>
           );
         })}
       </ul>
